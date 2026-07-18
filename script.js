8c3c2dcf6dbe153412c765435e91a4d8d95f7f08
Brynn Landry <luv.stuff97@gmail.com>
2026-07-18 09:24:05 -0400

    ep1
    added button, a thank you alert and hover colour

---
+++ script.js
@@ -0,0 +1,10 @@
+// Ep1 button logic
+const button = document.querySelector("#mainButton");
+
+//eventlistener
+button.addEventListener("click", ep1ButtonAction);
+
+//button function
+function ep1ButtonAction(){
+    alert("Thank you for clicking!")
+}
