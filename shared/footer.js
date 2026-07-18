1f9588271e5aeaec016fcc287b5965cda7cf9765
Brynn Landry <luv.stuff97@gmail.com>
2026-07-18 11:39:26 -0400

    adding a Kofi link

---
+++ shared/footer.js
@@ -0,0 +1,9 @@
+const footer = document.createElement('footer');
+
+footer.innerHTML = `
+<a href="https://ko-fi.com/clickmywayout" target="_blank" rel="noopener">
+    Support the Debt Payoff <3
+</a>
+`;
+
+document.body.appendChild(footer);
